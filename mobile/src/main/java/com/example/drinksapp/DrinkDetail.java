3f4bb2b5eade47f90a35fdcc89d872f4251d9986
Horacio Zorola <73261219+horaciozoroladev@users.noreply.github.com>
2022-07-07 14:53:22 -0500

    Se agrego DrinkDetail

---
+++ mobile/src/main/java/com/example/drinksapp/DrinkDetail.java
@@ -0,0 +1,14 @@
+package com.example.drinksapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrinkDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_detail);
+    }
+}
